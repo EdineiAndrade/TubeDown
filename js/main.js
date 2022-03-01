b6b6b2a1c0da0876f5fde0340002991df601be0a
EdineiAndrade <edinei22.ba@gmail.com>
2022-03-01 19:16:32 -0300

    Atualizando Funções JS

--- js/main.js
+++ js/main.js
@@ -2,24 +2,22 @@ const btn = document.querySelector("#btn-secon");
 
 btn.addEventListener("click", function(e){
     
-   
     e.preventDefault();
 
     const url1 = "http://54.145.59.200:3000/?url="
+    const url3 = "http://localhost:3000/?url="
+
     const url2 = document.querySelector("#send");
-    
-    const url = url1 + url2.value;
+    const url =  url1 + url2.value;
 
-
-    window.location.href = url;
     
-    alert(url);
-    console.log(url);
+        window.location.replace(url);
+   
 
     setTimeout(function(){
         console.log("Baixando");
         window.location.reload();
-    },5000);
+    },3000);
 
 });
 
