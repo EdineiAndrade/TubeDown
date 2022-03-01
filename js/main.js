bb4afdf4228b55fe7c64e30e1916ef855dedd6c8
EdineiAndrade <edinei22.ba@gmail.com>
2022-03-01 19:56:17 -0300

    Atualizando Funções JS

--- js/main.js
+++ js/main.js
@@ -9,15 +9,8 @@ btn.addEventListener("click", function(e){
 
     const url2 = document.querySelector("#send");
     const url =  url1 + url2.value;
-
     
     window.open(url, "_blank") || window.location.replace(url);
-   
-
-    setTimeout(function(){
-        console.log("Baixando");
-        window.location.reload();
-    },3000);
 
 });
 
