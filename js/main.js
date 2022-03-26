5949f2c35acfbb68d7b5684da6b4e695c11b2512
EdineiAndrade <edinei22.ba@gmail.com>
2022-03-26 20:13:11 -0300

    change Api for Heroku

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 const btn = document.querySelector("#btn-secon");
 
+
 btn.addEventListener("click", function(e){
     
     e.preventDefault();
 
-    const url1 = "http://54.145.59.200:3000/?url="
-    const url3 = "http://localhost:3000/?url="
+    //const url1 = "http://54.145.59.200:3000/?url="
+    const url2 = "https://app-tube-down.herokuapp.com/?url="
 
-    const url2 = document.querySelector("#send");
-    const url =  url1 + url2.value;
+    const urly = document.querySelector("#send");
+    const url =  url2 + urly.value;
     
     window.open(url, "_blank") || window.location.replace(url);
 
     setTimeout(function() {
         window.location.reload()
         document.querySelector("#send").value = ""
+        ur.setAttribute("href", url);
     }, 2000);
    
-    
-
 
 
 });
