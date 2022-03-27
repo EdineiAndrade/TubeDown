0d0db0aeb88df76c0e1ad39f4c9b7e5152c9d696
EdineiAndrade <edinei22.ba@gmail.com>
2022-03-27 12:58:27 -0300

    Config of Validation

--- js/main.js
+++ js/main.js
@@ -7,14 +7,17 @@ btn.addEventListener("click", function(e){
 
     //const url1 = "http://54.145.59.200:3000/?url="
     const url1 = "https://app-tube-down.herokuapp.com/?url="
-    const vry  =  "https://www.youtube.com/"
+    const vry1  =  "https://www.youtube.com/"
+    const vry2  = "https://youtu.be/"
     const url2 = document.querySelector("#send");
     const url =  url1 + url2.value;
     const urv1 = url2.value
     const urv2 = urv1.substr(0, 24)
+    const urv3 = url2.value
+    const urv4 = urv3.substr(0, 17)
 
-    if(urv2 === vry ){
-        window.open(url, "_blank") || window.location.replace(url);
+    if(urv2 === vry1 | urv4 === vry2  ){
+        window.open(url, "_blank");
 
     }else{
         alert('Link inválido. Copiar do Youtube!!')
