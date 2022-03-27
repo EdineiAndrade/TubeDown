63677a4dacbd20689ff79e1e7c3cf9c760a355ce
EdineiAndrade <edinei22.ba@gmail.com>
2022-03-27 12:37:51 -0300

    Inserting validation to URL

--- js/main.js
+++ js/main.js
@@ -6,20 +6,27 @@ btn.addEventListener("click", function(e){
     e.preventDefault();
 
     //const url1 = "http://54.145.59.200:3000/?url="
-    const url2 = "https://app-tube-down.herokuapp.com/?url="
+    const url1 = "https://app-tube-down.herokuapp.com/?url="
+    const vry  =  "https://www.youtube.com/"
+    const url2 = document.querySelector("#send");
+    const url =  url1 + url2.value;
+    const urv1 = url2.value
+    const urv2 = urv1.substr(0, 24)
 
-    const urly = document.querySelector("#send");
-    const url =  url2 + urly.value;
-    
-    window.open(url, "_blank") || window.location.replace(url);
+    if(urv2 === vry ){
+        window.open(url, "_blank") || window.location.replace(url);
+
+    }else{
+        alert('Link inválido. Copiar do Youtube!!')
+        
+    }
 
     setTimeout(function() {
         window.location.reload()
         document.querySelector("#send").value = ""
-        ur.setAttribute("href", url);
+        
     }, 2000);
    
-
 
 });
 
